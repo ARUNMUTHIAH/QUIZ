d53bd7bafded7e2a2420b69b29a31c01a9aaa902
Arun <msarun1999@gmail.com>
2023-03-04 11:52:46 +0530

    Introduced Data.

--- src/core/config.js
+++ src/core/config.js
@@ -1,3 +1,32 @@
-const config = {};
+/* eslint-disable max-len */
+const config = {
+	quizData: [
+		{
+			question: 'Which of the following is not a Javascript framework?',
+			options: ['Node', 'Vue', 'Flutter'],
+			answer: 'Flutter',
+		},
+		{
+			question: 'In which country did cricket originate?',
+			options: ['England', 'South Africa', 'Australia'],
+			answer: 'England',
+		},
+		{
+			question: 'How do you make the number one disappear?',
+			options: ['D', 'C', 'G'],
+			answer: 'G',
+		},
+		{
+			question: 'Which movie did Vijay and Ajith act together?',
+			options: ['3 idiots', 'Rajavin Parvaiyile', 'Selva'],
+			answer: 'Rajavin Parvaiyile',
+		},
+		{
+			question: 'If two’s company and three’s a crowd,what do four and five make?',
+			options: ['Twelve', 'Five', 'Nine'],
+			answer: 'Nine',
+		},
+	],
+};
 
 export default config;
