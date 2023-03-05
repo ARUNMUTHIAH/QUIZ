b1a089e33fb3e018a2d25ae66306d00f5ace6152
Arun <msarun1999@gmail.com>
2023-03-06 00:38:24 +0530

    Added Color to Options.

--- src/components/Body/Options.js
+++ src/components/Body/Options.js
@@ -1,18 +1,25 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { Box } from '@mui/material';
 
 const Options = (context) => {
-	const { state: { options }, setState } = context;
+	const { state: { options, userAnswer },
+		setState } = context;
 
-	return options.map((option, key) =>
-		<Box
-			key={ key }
-			{ ...{ className: 'option',
-				onClick: () => setState((state) => ({
-					...state,
-					userAnswer: option,
-				})) } }
-		>{option}</Box>);
+	return (
+		<Box>
+			{options.map((option, key) =>
+				<Box
+					key={ key }
+					{ ...{ className: userAnswer === option
+						? 'correctOption'
+						: 'option',
+					onClick: () => setState((state) => ({
+						...state,
+						userAnswer: option,
+					})) } }
+				>{option}</Box>)}
+		</Box>);
 };
 
 export default Options;
