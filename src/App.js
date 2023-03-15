2f080c00ec5119f6e0e98f8340407ed3bd65dc87
Arun <msarun1999@gmail.com>
2023-03-15 18:33:02 +0530

    InitialState added to seed.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 /* eslint-disable max-len */
 import './App.scss';
-import { React, useState } from 'react';
+import { React } from 'react';
 import Quiz from './components/Quiz';
 import { Box } from '@mui/material';
 
-const initialState = (context) => {
-	const { config: { inputData, categories }} = context;
-
-	return {
-		currentQuestion: 0,
-		currentCategory: 0,
-		userAnswer: null,
-		userInfo: categories,
-		option: '',
-		input: inputData,
-		score: 0,
-		user: false,
-		owner: false,
-		isActive: false,
-		isPlay: false,
-		isNext: false,
-		isFinish: false,
-	};
-};
-
-const App = (context) => {
-	const [state, setState] = useState(initialState(context));
-	const extendedContext = { ...context, state, setState };
-
-	return <Box className="App">
-		<Quiz { ...extendedContext }/>
+const App = (context) =>
+	<Box className="App">
+		<Quiz { ...context }/>
 	</Box>;
-};
 
 export default App;
--- src/core/seed.js
+++ src/core/seed.js
@@ -1,3 +1,19 @@
-const seed = {};
+import config from './config';
+
+const seed = {
+	currentQuestion: 0,
+	currentCategory: 0,
+	userAnswer: null,
+	userInfo: config.categories,
+	option: '',
+	input: config.inputData,
+	score: 0,
+	user: false,
+	owner: false,
+	isActive: false,
+	isPlay: false,
+	isNext: false,
+	isFinish: false,
+};
 
 export default seed;
