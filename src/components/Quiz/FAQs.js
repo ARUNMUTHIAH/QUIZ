8619b50d062cd6d46b430393d8f53ed35f929d29
Arun <msarun1999@gmail.com>
2023-04-13 17:14:02 +0530

    Introduced FAQS,AboutUs.

---
+++ src/components/Quiz/FAQs.js
@@ -0,0 +1,8 @@
+// Question : Is this website free or do I have to pay to access the quizzes?
+// Answer : The website is free. You can access quizzes and practice tests with your free account
+
+// Question : Can I print, copy or email my Quiz questions?
+// Answer :No
+
+// Question :Can I take a practise test more than once?
+// Answer : Yes
