a70b427208fc432708a4cc458b102eae86775cc8
Arun <msarun1999@gmail.com>
2023-04-21 12:58:24 +0530

    SignUp page Introduced.

--- src/components/Quiz/UserInfo/SignUp/Button/SignUp.js
+++ src/components/Quiz/UserInfo/SignUp/Button/SignUp.js
@@ -1,10 +1,14 @@
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import React from 'react';
 
 const SignUp = () =>
-	<Button
-		variant="contained"
-		color="error"
-	> SignUp</Button>;
+	<Box>
+		<Button
+			className="signUpPageButton"
+			variant="contained"
+			color="error"
+		> SignUp </Button>
+		<h3 className="singUpLabel">Already have an account ?</h3>
+	</Box>;
 
 export default SignUp;
